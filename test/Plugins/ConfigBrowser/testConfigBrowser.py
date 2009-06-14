0f65e47b0a371b045b3fd957e36ca286c1c454ab
Andreas Hinzmann <sha1-5d7a3aa3734fdea6829a5f92e42cf5075d63a16b@cern.ch>
2009-06-14 16:58:36 +0000

    update tests for CMSSW_3_1_X

--- test/Plugins/ConfigBrowser/testConfigBrowser.py
+++ test/Plugins/ConfigBrowser/testConfigBrowser.py
@@ -8,17 +8,25 @@ logging.root.setLevel(logging.DEBUG)
 
 import Path
 from Vispa.Main.Directories import *
-examplesDirectory = os.path.abspath(os.path.join(os.path.join(baseDirectory,".."),"examples"))
+examplesDirectory = os.path.join(baseDirectory,"examples/Plugins/ConfigBrowser")
+test=1
+if not os.path.exists(examplesDirectory):
+    examplesDirectory = os.path.abspath(os.path.join(os.path.join(baseDirectory,".."),"examples"))
+    test=2
 
 from Vispa.Main.Application import *
 from Vispa.Main import Profiling
 
 class ConfigBrowserTestCase(unittest.TestCase):
     def testConfigBrowser(self):
+        global test
         logging.debug(self.__class__.__name__ +': testRun()')
         self.app=Application(sys.argv)
         self.app.mainWindow().setWindowTitle("test ConfigBrowser")
-        self.app.openFile(os.path.join(examplesDirectory,"patLayer1_fromAOD_full_cfg_CMSSW_3_1_X.py"))
+        if test==1:
+            self.app.openFile(os.path.join(examplesDirectory,"patLayer1_fromLayer0_full_cfg_CMSSW_2_1_X.py"))
+        if test==2:
+            self.app.openFile(os.path.join(examplesDirectory,"patLayer1_fromAOD_full_cfg_CMSSW_3_1_X.py"))
         self.app.run()
 
 if __name__ == "__main__":
--- test/Plugins/ConfigBrowser/testConfigDataAccessor.py
+++ test/Plugins/ConfigBrowser/testConfigDataAccessor.py
@@ -9,7 +9,11 @@ logging.root.setLevel(logging.DEBUG)
 import Path
 from Vispa.Main.Directories import *
 sys.path.append(os.path.join(baseDirectory,"Vispa/Plugins/ConfigBrowser"))
-examplesDirectory = os.path.abspath(os.path.join(os.path.join(baseDirectory,".."),"examples"))
+examplesDirectory = os.path.join(baseDirectory,"examples/Plugins/ConfigBrowser")
+test=1
+if not os.path.exists(examplesDirectory):
+    examplesDirectory = os.path.abspath(os.path.join(os.path.join(baseDirectory,".."),"examples"))
+    test=2
 
 from Vispa.Main.Exceptions import *
 try:
@@ -20,29 +24,32 @@ from Vispa.Main import Profiling
 
 class ConfigDataAccessorTestCase(unittest.TestCase):
     def testExample(self):
+        global test
         logging.debug(self.__class__.__name__ +': testExample()')
 
         accessor=ConfigDataAccessor()
-        accessor.open(os.path.join(examplesDirectory,"cleanLayer1Objects_cff_CMSSW_3_1_X.py"))
+        if test==1:
+            accessor.open(os.path.join(examplesDirectory,"patLayer1_cff_CMSSW_2_1_X.py"))
 
-        self.assertEqual(len(accessor.topLevelObjects()),2)
-        self.assertEqual(len(accessor.properties(accessor.topLevelObjects()[0])),7)
-        self.assertEqual(len(accessor.children(accessor.topLevelObjects()[0])),7)
-        self.assertEqual(accessor.label(accessor.topLevelObjects()[0]),"cleanLayer1Objects")
+            self.assertEqual(len(accessor.topLevelObjects()),2)
+            self.assertEqual(len(accessor.properties(accessor.topLevelObjects()[0])),8)
+            self.assertEqual(len(accessor.children(accessor.topLevelObjects()[0])),8)
+            self.assertEqual(accessor.label(accessor.topLevelObjects()[0]),"allObjects")
 
-#        accessor.open(os.path.join(examplesDirectory,"patLayer1_cff_CMSSW_2_1_X.py"))
+            accessor.open(os.path.join(examplesDirectory,"patLayer1_fromLayer0_full_cfg_CMSSW_2_1_X.py"))
 
-#        self.assertEqual(len(accessor.topLevelObjects()),2)
-#        self.assertEqual(len(accessor.properties(accessor.topLevelObjects()[0])),8)
-#        self.assertEqual(len(accessor.children(accessor.topLevelObjects()[0])),8)
-#        self.assertEqual(accessor.label(accessor.topLevelObjects()[0]),"allObjects")
+            self.assertEqual(len(accessor.children(accessor.topLevelObjects()[0])),9)
+            self.assertEqual(len(accessor.properties(accessor.children(accessor.topLevelObjects()[0])[0])),5)
+            self.assertEqual(len(accessor.children(accessor.children(accessor.topLevelObjects()[0])[0])),1)
+            self.assertEqual(accessor.label(accessor.children(accessor.topLevelObjects()[0])[0]),"source")
+            
+        if test==2:
+            accessor.open(os.path.join(examplesDirectory,"cleanLayer1Objects_cff_CMSSW_3_1_X.py"))
 
-#        accessor.open(os.path.join(examplesDirectory,"patLayer1_fromLayer0_full_cfg_CMSSW_2_1_X.py"))
-
-#        self.assertEqual(len(accessor.children(accessor.topLevelObjects()[0])),9)
-#        self.assertEqual(len(accessor.properties(accessor.children(accessor.topLevelObjects()[0])[0])),5)
-#        self.assertEqual(len(accessor.children(accessor.children(accessor.topLevelObjects()[0])[0])),1)
-#        self.assertEqual(accessor.label(accessor.children(accessor.topLevelObjects()[0])[0]),"source")
+            self.assertEqual(len(accessor.topLevelObjects()),2)
+            self.assertEqual(len(accessor.properties(accessor.topLevelObjects()[0])),7)
+            self.assertEqual(len(accessor.children(accessor.topLevelObjects()[0])),7)
+            self.assertEqual(accessor.label(accessor.topLevelObjects()[0]),"cleanLayer1Objects")
 
 if __name__ == "__main__":
     Profiling.analyze("unittest.main()",__file__)
